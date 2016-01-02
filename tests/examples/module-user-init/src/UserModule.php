c0bc9ff16761f73d163142499c5684ca2395480a
gossi <github@gos.si>
2016-01-02 15:47:53 +0100

    forget one file, huh?

---
+++ tests/examples/module-user-init/src/UserModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace keeko\user;
+
+use keeko\core\package\AbstractModule;
+
+/**
+ * Keeko User Module
+ * 
+ * @license MIT
+ * @author Tester
+ */
+class UserModule extends AbstractModule {
+
+	/**
+	 */
+	public function install() {
+	}
+
+	/**
+	 */
+	public function uninstall() {
+	}
+
+	/**
+	 * @param mixed $from
+	 * @param mixed $to
+	 */
+	public function update($from, $to) {
+	}
+}
